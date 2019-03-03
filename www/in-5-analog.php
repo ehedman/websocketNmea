0fc74f73dbf22f53391f76e953bc7fc6af5e8f34
Erland Hedman <erland@hedmanshome.se>
2019-03-03 20:42:24 +0100

    Minor css style changes

--- www/in-5-analog.php
+++ www/in-5-analog.php
@@ -24,7 +24,6 @@
         {
             background:transparent;
             padding:0;
-            margin: 165px 0 0 0;
             width: 100%;
             overflow: hidden;
         }
@@ -32,43 +31,47 @@
         #next-butt
         {
             z-index:100;
-            position:fixed;
-            top:165px;
-            left:0px;
+            position:relative;
+            top:175px;
+            margin-left:100px;
             height:40px;
             width:40px;
             font-weight: bold;
             border-radius:50%;
             -moz-border-radius:50%;
         }
-         #chart-butt
+        #chart-butt
         {
             z-index:100;
-            position:fixed;
-            top:165px;
-            left:238px;
+            position:relative;
+            top:175px;
+            margin-left:246px;
             height:40px;
             width:40px;
             border-radius:50%;
             -moz-border-radius:50%;
             font-weight: bold;
         }
-        </style>
+
+     </style>
 
     </head>
     <body onload="init()">
-    <div>
+    <div id="adc_main_div">
         <div id="adc_left_div">   
             <iframe width="256" height="256" frameborder="0" border="0" cellspacing="0" scrolling=no src="in-8-volt.php" id="adc_left_fr">
                 <p>Your browser does not support iframes.</p>
             </iframe>
         </div>
-         <div id="adc_center_div">       
-            <iframe  width="256" height="256" frameborder="0" border="0" cellspacing="0" scrolling=no src="in-9-curr.php" id="adc_center_fr"></iframe>
+        <div id=adc_r2_div>
+            <div id="adc_right_div">       
+                <iframe  width="256" height="256" frameborder="0" border="0" cellspacing="0" scrolling=no src="in-10-temp.php" id="adc_right_fr"></iframe>
+            </div>
+
+             <div id="adc_center_div">       
+                <iframe  width="256" height="256" frameborder="0" border="0" cellspacing="0" scrolling=no src="in-9-curr.php" id="adc_center_fr"></iframe>
+            </div>
         </div>
-        <div id="adc_right_div">       
-            <iframe  width="256" height="256" frameborder="0" border="0" cellspacing="0" scrolling=no src="in-10-temp.php" id="adc_right_fr"></iframe>
-        </div> 
         <input id="next-butt" type="button" value="N" title="Click to shift instrument" onclick="nextinstrument();">
         <input id="chart-butt" type="button" value="P" title="Click to show Power Chart" onclick="openInNewTab('power.php');">
     </div>
